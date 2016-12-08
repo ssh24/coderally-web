d9cee35b0cd843c95aad478bd9d70c10304261d5
ssh24 <sadman.hasan99@live.com>
2016-12-08 12:16:54 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "loopback",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-extra-semi": "error",
+        "no-else-return": "error"
+    }
+};
---
+++ server/boot/build-models.js
@@ -0,0 +1,92 @@
+/* don't use this file without admin privileges */
+
+// 'use strict';
+//
+// var async = require('async');
+// var fs = require('fs');
+// var path = require('path');
+//
+// var trackVehicles = [];
+// var vehicles = [];
+//
+// var Circuit =
+//   fs.readFileSync('./game-assets/tracks/circuit/trackInfo/vehicleInfo.json');
+// var Desert =
+//   fs.readFileSync('./game-assets/tracks/desert/trackInfo/vehicleInfo.json');
+// var Desk =
+//   fs.readFileSync('./game-assets/tracks/desk/trackInfo/vehicleInfo.json');
+// var Figure8 =
+//   fs.readFileSync('./game-assets/tracks/figure8/trackInfo/vehicleInfo.json');
+// var LowEarthOrbit =
+//   fs.readFileSync
+//   ('./game-assets/tracks/lowearthorbit/trackInfo/vehicleInfo.json');
+// var Pond =
+//   fs.readFileSync('./game-assets/tracks/pond/trackInfo/vehicleInfo.json');
+// var Sky =
+//   fs.readFileSync('./game-assets/tracks/sky/trackInfo/vehicleInfo.json');
+// var Space =
+//   fs.readFileSync('./game-assets/tracks/space/trackInfo/vehicleInfo.json');
+// var Water =
+//   fs.readFileSync('./game-assets/tracks/water/trackInfo/vehicleInfo.json');
+//
+// module.exports = function(app) {
+//   var datasource = app.dataSources.db;
+//
+//   var tracks = [{
+//     tName: 'Circuit',
+//   }, {
+//     tName: 'Desert',
+//   }, {
+//     tName: 'Desk',
+//   }, {
+//     tName: 'Figure8',
+//   }, {
+//     tName: 'LowEarthOrbit',
+//   }, {
+//     tName: 'Pond',
+//   }, {
+//     tName: 'Sky',
+//   }, {
+//     tName: 'Space',
+//   }, {
+//     tName: 'Water',
+//   }];
+//
+//   datasource.automigrate('Track', function(err) {
+//     if (err) return cb(err);
+//     var Track = app.models.Track;
+//     Track.create(tracks);
+//   });
+//
+//   var circuitVehicles = JSON.parse(Circuit).vehicles;
+//   var desertVehicles = JSON.parse(Desert).vehicles;
+//   var deskVehicles = JSON.parse(Desk).vehicles;
+//   var figure8Vehicles = JSON.parse(Figure8).vehicles;
+//   var lowearthorbitVehicles = JSON.parse(LowEarthOrbit).vehicles;
+//   var pondVehicles = JSON.parse(Pond).vehicles;
+//   var skyVehicles = JSON.parse(Sky).vehicles;
+//   var spaceVehicles = JSON.parse(Space).vehicles;
+//   var waterVehicles = JSON.parse(Water).vehicles;
+//
+//   trackVehicles.push(circuitVehicles);
+//   trackVehicles.push(desertVehicles);
+//   trackVehicles.push(deskVehicles);
+//   trackVehicles.push(figure8Vehicles);
+//   trackVehicles.push(lowearthorbitVehicles);
+//   trackVehicles.push(pondVehicles);
+//   trackVehicles.push(skyVehicles);
+//   trackVehicles.push(spaceVehicles);
+//   trackVehicles.push(waterVehicles);
+//
+//   for (var i = 0; i < trackVehicles.length; i++) {
+//     for (var j = 0; j < trackVehicles[i].length; j++) {
+//       vehicles.push(trackVehicles[i][j]);
+//     }
+//   }
+//
+//   datasource.automigrate('Vehicle', function(err) {
+//     if (err) return cb(err);
+//     var Vehicle = app.models.Vehicle;
+//     Vehicle.create(vehicles);
+//   });
+// };
