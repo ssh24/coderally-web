0e36b2e0b80e13eb694ff72ad33dd39a691c2216
ssh24 <sadman.hasan99@live.com>
2016-12-22 12:30:35 -0500

    Initial start to select vehicle module
    Fixed styling issues on css
    Added select-vehicle service on index
    Modified select-track controller for forbidden checks
    Added controls to select vehicle
    Added selectvehicle service to store the selected vehicle
    Modified next button name on select-track.html
    Added views on select-vehicle.html
    Fixed image dir for game asset vehicles
    Updated the database with recent data

---
+++ client/js/controllers/select-vehicle.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/** select track controller **/
+
+angular.module('app')
+  .controller('SelectVehicleController', ['$scope', 'Vehicle', '$state', 'User',
+    'SelectedTrack', 'SelectedVehicle',
+    function($scope, Vehicle, $state, User, SelectedTrack, SelectedVehicle) {
+      $scope.username = User; // set username for account info
+      $scope.chosenTrack = SelectedTrack; // set selected track for getting vehicle info
+      $scope.vehicle = SelectedVehicle; // selected vehicle
+      $scope.vehicle.chosen = ''; // chosen vehicle
+
+      // check to see if user is logged in, if not redirect to forbidden
+      if ($scope.username.text == undefined || $scope.username.text == '')
+        $state.go('forbidden'); // go to forbidden page
+      // else continue on select track page
+      else {
+        $scope.isDisabledNextSelectVehicle = true; // have next button disabled by default
+
+        // logout function
+        $scope.logout = function() {
+          $state.go('login'); // go back to login page once logged out
+        };
+
+        // find all the vehicles filtering by the chosen track id
+        $scope.vehicles = Vehicle.find({
+          filter: {
+            where: {
+              trackId: $scope.chosenTrack.chosen.id,
+            },
+          },
+        });
+
+        // upon selection of tracks tracks
+        $scope.toggleVehicles = function(vehicle) {
+          $scope.selected = vehicle; // set the selected track border
+          $scope.vehicle.chosen = vehicle; // set the selected vehicle
+          $scope.isDisabledNextSelectVehicle = false; // enable next button
+        };
+
+        // when clicking back from choosing vehicle page
+        $scope.previousSelectVehicle = function() {
+          if ($scope.username.text == undefined || $scope.username.text == '')
+            $state.go('forbidden'); // go to forbidden page
+          else
+            $state.go('select-track');
+        };
+
+        // when clicking next after choosing a vehicle
+        $scope.nextSelectVehicle = function() {
+          if ($scope.username.text == undefined || $scope.username.text == '')
+            $state.go('forbidden'); // go to forbidden page
+          // else
+          //   $state.go('modify-code');
+        };
+      }
+    },
+  ]);
