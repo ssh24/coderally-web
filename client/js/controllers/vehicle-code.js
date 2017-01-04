7f5c06cfeb1d98281680d1fec09e622c78e3fe62
ssh24 <sadman.hasan99@live.com>
2017-01-04 15:05:49 -0500

    Completed getting access to car file
    Reloacted game-assets
    Added logout, previous, next and onLoad functions on vehicle-code
    Updated the database tables with latest data

--- client/js/controllers/vehicle-code.js
+++ client/js/controllers/vehicle-code.js
@@ -3,9 +3,9 @@
 /** vehicle code controller **/
 
 angular.module('app')
-  .controller('VehicleCodeController', ['$scope', '$state', 'User',
+  .controller('VehicleCodeController', ['$scope', '$state', '$http', 'User',
     'SelectedTrack', 'SelectedVehicle',
-    function($scope, $state, User, SelectedTrack, SelectedVehicle) {
+    function($scope, $state, $http, User, SelectedTrack, SelectedVehicle) {
       $scope.username = User; // get username info
       $scope.track = SelectedTrack; // get selected track info
       $scope.vehicle = SelectedVehicle; // get selected vehicle info
@@ -13,11 +13,43 @@ angular.module('app')
       // check to see if user is logged in, if not redirect to forbidden
       if ($scope.username.text == undefined || $scope.username.text == '')
         $state.go('forbidden'); // go to forbidden page
-      // else continue on select track page
+      // else continue on vehicle code page
       else {
-        $scope.aceLoaded = function(_editor) {
-          // Options
-          _editor.setReadOnly(true);
+        $scope.isDisabledNextVehicleCode = true; // have next button disabled by default
+
+        // logout function
+        $scope.logout = function() {
+          $state.go('login'); // go back to login page once logged out
+        };
+
+        // upon ace loaded
+        $scope.aceLoaded = function(editor) {
+          $http.get($scope.vehicle.chosen.vInfo)
+            .success(function(data) {
+              editor.$blockScrolling = Infinity;
+              editor.setValue(JSON.stringify(data));
+              var val = editor.session.getValue();
+              var o = JSON.parse(val);
+              val = JSON.stringify(o, null, 4);
+              editor.setValue(val);
+            })
+            .error(function(data) {});
+        };
+
+        // when clicking back from vehicle code page
+        $scope.previousVehicleCode = function() {
+          if ($scope.username.text == undefined || $scope.username.text == '')
+            $state.go('forbidden'); // go to forbidden page
+          else
+            $state.go('select-vehicle');
+        };
+
+        // when clicking next after modifying code
+        $scope.nextVehicleCode = function() {
+          if ($scope.username.text == undefined || $scope.username.text == '')
+            $state.go('forbidden'); // go to forbidden page
+          else {}
+          // $state.go('vehicle-code');
         };
       }
     },
